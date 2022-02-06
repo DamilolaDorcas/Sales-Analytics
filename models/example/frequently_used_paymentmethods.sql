29e94945dca5a2d8a78e078dedaffebdf6cabcc4
Damilola <dhurcasdamilola15@gmail.com>
2022-02-06 15:48:52 +0100

    freq_used_paymentmethod

--- models/example/frequently_used_paymentmethods.sql
+++ models/example/frequently_used_paymentmethods.sql
@@ -1,9 +1,9 @@
--- {{ config(materialized = 'table') }}
+{{ config(materialized = 'table') }}
 -- select distinct paymentmethod as diff_payment_methods
 -- from {{ source('stripe', 'payment') }}
 
- select paymentmethod, count(1) as different_paymentmethods from
+ select paymentmethod, count(1) as frequently_used_paymentmethods from
   {{ source('stripe', 'payment') }}
   group by paymentmethod
-  order by different_paymentmethods
+  order by frequently_used_paymentmethods
 
