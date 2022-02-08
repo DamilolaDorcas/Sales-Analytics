6508df93c062570f1800e4029ddd4243578b3439
Damolola Dorcas <dhurcasd@gmail.com>
2022-02-08 15:29:21 +0000

    testing dbt envionment

---
+++ models/test.sql
@@ -0,0 +1,7 @@
+   select
+    min(amount) as min_payment,
+    max(amount) as max_payment, 
+    count(orderid) as number_of_orders, 
+    avg(amount) as avg_payment,
+    sum(amount) as total_payment
+    from {{ source('stripe', 'payment') }}
